e91693c16b280764738d02553b59ad07c0acb154
RGBKnights <master@rgbknights.com>
2024-10-07 20:56:16 -0400

    reset

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using ElectricRaspberry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddLogging(configure => configure.AddConsole());
+builder.Services.AddHttpClient();
+builder.Services.AddHostedService<Application>();
+
+using var host = builder.Build();
+await host.RunAsync();
+
