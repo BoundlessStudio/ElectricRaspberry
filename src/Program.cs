23e14b58758ddf684018d4f19129c014e8a7d1cb
RGBKnights <master@rgbknights.com>
2024-11-05 09:38:20 -0500

    Cleanup

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,17 @@
-﻿using ElectricRaspberry;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenAI;
+using OpenAI.Audio;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddLogging(configure => configure.AddConsole());
 builder.Services.AddHttpClient();
-builder.Services.AddHostedService<Application>();
+builder.Services.AddSingleton(new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<OpenAIClient>().GetChatClient("gpt-4o-mini"));
+builder.Services.AddKeyedSingleton("tts", (sp, key) => sp.GetRequiredService<OpenAIClient>().GetAudioClient("tts-1"));
+builder.Services.AddKeyedSingleton("stt", (sp, key) => sp.GetRequiredService<OpenAIClient>().GetAudioClient("whisper-1"));
 
 using var host = builder.Build();
 await host.RunAsync();
